3b78142b1c8ebb8db34f203ebe6b1715ba714376
brodie-m <brodiedmcguire@gmail.com>
2021-10-21 10:15:48 +0100

    added icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,21 @@
 import React from 'react'
-
+import {BsPlus, BsFillLightningFill, BsGearFill} from 'react-icons/bs'
+import {FaFire, FaPoo} from 'react-icons/fa'
 const Sidebar = () => {
     return (
         <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <SideBarIcon icon={<FaFire size ='28'/>}/>
+            <SideBarIcon icon={<BsPlus size ='28'/>}/>
+            <SideBarIcon icon={<BsFillLightningFill size ='28'/>}/>
+            <SideBarIcon icon={<FaPoo size ='28'/>}/>
+            
         </div>
     )
 }
+const SideBarIcon = ({icon}) => (
+    <div className = 'sidebar-icon'>
+        {icon}
+    </div>
+)
 
 export default Sidebar
